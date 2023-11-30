92bcfa512d29091f49b0bef16049d167f9dc9f7c
Rainer Fritz <rain3rfritz@gmail.com>
2023-11-30 22:31:56 +0100

    merge main

--- src/nrf52/nrf52_main.cpp
+++ src/nrf52/nrf52_main.cpp
@@ -775,7 +775,7 @@ void nrf52loop()
         bPosFirst = false;
         posinfo_shot=false;
         
-        sendPosition(posinfo_interval, meshcom_settings.node_lat, meshcom_settings.node_lat_c, meshcom_settings.node_lon, meshcom_settings.node_lon_c, meshcom_settings.node_alt, meshcom_settings.node_press, meshcom_settings.node_hum, meshcom_settings.node_temp, meshcom_settings.node_temp2, meshcom_settings.node_press_alt, meshcom_settings.node_press_asl);
+        sendPosition(posinfo_interval, meshcom_settings.node_lat, meshcom_settings.node_lat_c, meshcom_settings.node_lon, meshcom_settings.node_lon_c, meshcom_settings.node_alt, meshcom_settings.node_press, meshcom_settings.node_hum, meshcom_settings.node_temp, meshcom_settings.node_temp2, meshcom_settings.node_press_alt, meshcom_settings.node_press_asl, meshcom_settings.node_gas_res);
 
         posinfo_last_lat=posinfo_lat;
         posinfo_last_lon=posinfo_lon;
