562a05158eb6b7b2873cd1200210c0c039c5dc98
Kurt <kbaumann@chello.at>
2026-07-13 18:37:17 +0200

    v4.35p sat mode

--- src/lora_setchip.cpp
+++ src/lora_setchip.cpp
@@ -54,8 +54,8 @@
 bool rf_crc = true;
 uint16_t rf_preamble_length = LORA_PREAMBLE_LENGTH;
 
-//0...EU  1...UK, 2...ON, 3...EA, 4...OM, 8...EU8, 10...US, ..... 18...868, 19...915, 20...MAN
-String strCountry[max_country] = {"EU", "UK", "ON", "EA", "LA", "868", "915", "MAN", "EU8", "UK8", "US", "VR2", "none", "none", "none", "none"};
+//0...EU  1...UK, 2...ON, 3...EA, 4...OM, 8...EU8, 10...US, ..... 5...868, 6...915, 7...MAN
+String strCountry[max_country] = {"EU", "UK", "ON", "EA", "LA", "868", "915", "MAN", "EU8", "UK8", "US", "VR2", "435", "436", "442", "none"};
 
 String getCountry(int iCtry)
 {
@@ -272,7 +272,7 @@ void lora_setcountry(int iCtry)
 
             break;
 
-        case 6:  // 906 ... 
+        case 6:  // 915 ... 
 
             #if defined(BOARD_RAK4630) || defined(USE_HELTEC_T114) || defined(BOARD_T_ECHO)
                 meshcom_settings.node_freq = 906875000;
@@ -405,6 +405,55 @@ void lora_setcountry(int iCtry)
             
             meshcom_settings.node_preamplebits = 8;
 
+            break;
+
+        case 12:  // 435 ... 
+            #if defined(BOARD_RAK4630) || defined(USE_HELTEC_T114) || defined(BOARD_T_ECHO)
+                meshcom_settings.node_freq = 435750000;
+                meshcom_settings.node_bw = 1;
+                meshcom_settings.node_cr = 2;
+            #else
+                meshcom_settings.node_freq = 435.750;
+                meshcom_settings.node_bw = 250.0;
+                meshcom_settings.node_cr = 6;
+            #endif
+            meshcom_settings.node_sf = 11;
+
+            meshcom_settings.node_track_freq = LORA_APRS_FREQUENCY;
+            
+            meshcom_settings.node_preamplebits = 8;
+            break;
+        case 13:  // 436 ... 
+            #if defined(BOARD_RAK4630) || defined(USE_HELTEC_T114) || defined(BOARD_T_ECHO)
+                meshcom_settings.node_freq = 436250000;
+                meshcom_settings.node_bw = 1;
+                meshcom_settings.node_cr = 2;
+            #else
+                meshcom_settings.node_freq = 436.250;
+                meshcom_settings.node_bw = 250.0;
+                meshcom_settings.node_cr = 6;
+            #endif
+            meshcom_settings.node_sf = 11;
+
+            meshcom_settings.node_track_freq = LORA_APRS_FREQUENCY;
+            
+            meshcom_settings.node_preamplebits = 8;
+            break;
+        case 14:  // 442 ... 
+            #if defined(BOARD_RAK4630) || defined(USE_HELTEC_T114) || defined(BOARD_T_ECHO)
+                meshcom_settings.node_freq = 442000000;
+                meshcom_settings.node_bw = 1;
+                meshcom_settings.node_cr = 2;
+            #else
+                meshcom_settings.node_freq = 442.000;
+                meshcom_settings.node_bw = 250.0;
+                meshcom_settings.node_cr = 6;
+            #endif
+            meshcom_settings.node_sf = 11;
+
+            meshcom_settings.node_track_freq = LORA_APRS_FREQUENCY;
+            
+            meshcom_settings.node_preamplebits = 8;
             break;
 
         default:    // EU
